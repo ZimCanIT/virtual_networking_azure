b817c88de050feb890fa4825531520bbf5e6d427
ZimCanIT <77082071+ZimCanIT@users.noreply.github.com>
2022-11-17 21:53:36 +0000

    resource group created

---
+++ infra/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id = var.tenant_id
+}
+
+# virtual netwkorking
+resource "azurerm_resource_group" "az104_vnet_rg" {
+  name     = "az104-04-rg1"
+  location = "West Europe"
+}
---
+++ infra/variables/variables.tf
@@ -0,0 +1,8 @@
+variable "tenant_id" {
+    type = string
+    description = "ZimCanIT org tenant id"
+}
+variable "subscription_id" {
+    type = string
+    description = "Subscription within which resources are deployed"
+}
