35bddbd49047e692a83ef5191f1e987835622411
ZimCanIT <77082071+ZimCanIT@users.noreply.github.com>
2022-11-18 09:26:47 +0000

    resource group deployed

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,17 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+  features {
+    # preventing deletion of resource group if the rg contains resources
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+    # requesting a graceful shutdown and ensuring os disk is deleted on deletion of vm resource
+    virtual_machine {
+      delete_os_disk_on_deletion = true
+      graceful_shutdown          = true
+    }
+  }
 
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
@@ -17,6 +27,8 @@ provider "azurerm" {
 
 # virtual netwkorking
 resource "azurerm_resource_group" "az104_vnet_rg" {
-  name     = "az104-04-rg1"
+  name     = "${local.resource_prefix}rg1"
   location = "West Europe"
+
+  tags = local.common_tags
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -6,3 +6,15 @@ variable "subscription_id" {
   type        = string
   description = "Subscription within which resources are deployed"
 }
+#-------------------------------------------------------------
+
+locals {
+  # refix for naming convention across all resources
+  resource_prefix = "az104-04-"
+
+  common_tags = {
+    env   = "training"
+    lab   = "az-104-lab-4"
+    owner = "zimcanit"
+  }
+}
